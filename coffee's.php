de33938fd65ad70e81ea1b457a76362c2d2b1264
Marievnm7 <162945520+Marievnm7@users.noreply.github.com>
2024-04-18 15:54:02 +0800

    Add files via upload

---
+++ coffee's.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffee</title>
+</head>
+<style>
+
+      
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 30%;
+  margin-bottom: 10px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  padding: 5px;
+  text-align: center;
+  color: #474e5d;
+}
+
+.container {
+  padding: 15 10px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+
+@media screen and (max-width: 500px) {
+  .column {
+    width: 60%;
+    display: block;
+  }
+}
+</style>
+
+<body>
+<?php include 'home.php' ?>
+
+<div class="about-section">
+  <h3>List of Coffee's</h3>
+</div>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="vanilla.jpg" alt="vanilla" style="width:100%">
+      <div class="container">
+        <h2>Vanilla</h2>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="mocha.jpg" alt="mocha" style="width:100%">
+      <div class="container">
+        <h2>Mocha</h2>
+       
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <img src="chocolate.jpg" alt="chocolate" style="width:100%">
+      <div class="container">
+        <h2>chocolate</h2>
+        
+        <div class="column">
+    <div class="card">
+      <img src="cofe.jpg" alt="cofe" style="width:100%">
+      <div class="container">
+        <h2>chocolate</h2>
+        
+        <div class="column">
+    <div class="card">
+      <img src="ice.jpg" alt="ice" style="width:100%">
+      <div class="container">
+        <h2>chocolate</h2>
+        
+        <div class="column">
+    <div class="card">
+      <img src="caffee.jpg" alt="caffee" style="width:100%">
+      <div class="container">
+        <h2>chocolate</h2>
+        
+
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
